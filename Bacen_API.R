89ea8819c379125faa60186f59c9548cc3d90826
Paulo Icaro <87305764+paulo-icaro@users.noreply.github.com>
2024-05-08 14:45:35 -0300

    Ajuste no flag de conexão

--- Bacen_API.R
+++ Bacen_API.R
@@ -14,14 +14,6 @@
 # https://dadosabertos.bcb.gov.br/dataset/10844-indice-de-precos-ao-consumidor-amplo-ipca---servicos/resource/c0980df7-ad92-47af-b71c-790825f4710a
 
 
-# -------------------- #
-# --- URL's Modelo --- #
-# -------------------- #
-# url_ipca_br = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json&dataInicial=01/01/2003&dataFinal=01/12/2023'
-# url_ipca_ne = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.13921/dados?formato=json&dataInicial=01/01/2003&dataFinal=01/12/2023'
-# url_ipca_for = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.13006/dados?formato=json&dataInicial=01/01/2003&dataFinal=01/12/2023'
-
-
 
 # ---------------------------- #
 # --- Packages e Libraries --- #
@@ -34,14 +26,13 @@ library(dplyr)                              # Biblioteca para manipulacao de dad
 
 
 
-
-
 # ------------------------------ #
 # --- API - Funcao de Coleta --- #
 # ------------------------------ #
 
 bacen_api = function(url, httr = TRUE){
-  message('Iniciando a conexao com a API')
+  message('Iniciando a conexao com a API do Bacen\n')
+  flag = 0
   
   # --- Conexao API - Utilizando httr --- #
   if(httr == TRUE){
@@ -52,24 +43,30 @@ bacen_api = function(url, httr = TRUE){
     
     # --- Flag de Conexao --- #
     if(api_connection$status_code == 200){
-      message('Conexao bem sucedida ! \nDados sendo coletados ...\n')
+      dlg_message(message = 'Conexao bem sucedida ! \nDados sendo coletados ...\n', type = 'ok')
+    } 
+    else if(api_connection$status_code != 200){
+      while(api_connection$status_code != 200 & flag <= 3){
+        flag = flag + 1
+        
+        if(flag == 1){
+          Sys.sleep(2)
+          message('Problemas na conexao. \nTentando acessar a API novamente ...\n')}
+        if(flag == 2){
+          Sys.sleep(5)
+          message('Problemas na conexao. \nTentando acessar a API novamente ...\n')}
+        if(flag == 3){
+          Sys.sleep(10)
+          message('Problemas na conexao. \nTentando acessar a API uma última vez ...\n')}
+        
+        api_connection = GET(url = url)        
+      }
+      
+      ifelse(api_connection$status_code == 200,
+             dlg_message(message = 'Conexao bem sucedida ! \nDados sendo coletados ...\n', type = 'ok'),
+             dlg_message(message = 'Falha na conexao ! \nTente conectar com a API mais tarde.', type = 'ok')
+      )
     }
-    if(api_connection$status_code != 200){
-      message('Problemas na conexao \nTentando acessar a API novamente ...\n')
-      Sys.sleep(2)
-      api_connection = GET(url = url)
-    }
-    if(api_connection$status_code != 200){
-      message('Problemas na conexao \nTentando acessar a API novamente ...\n')
-      Sys.sleep(5)
-      api_connection = GET(url = url)
-    }
-    if(api_connection$status_code != 200){
-      message('Problemas na conexao \nTentando acessar a API uma última vez ...\n')
-      Sys.sleep(10)
-      api_connection = GET(url = url)
-    }
-    else{'Falha na conexao :(\nTente conectar com a API mais tarde'}
     
     
     # --- Conversao dos Dados para Formato Legivel --- #
@@ -83,33 +80,37 @@ bacen_api = function(url, httr = TRUE){
   else{
     
     # -- Conexao com API -- # 
-    api_connection =
-      request(base_url = url) %>%
-      req_perform()
+    api_connection = request(base_url = url) %>% req_perform()
     
     
-    # -- Flag de conexao -- #
+    # --- Flag de Conexao --- #
     if(api_connection$status_code == 200){
-      message('Conexao bem sucedida ! \nDados sendo coletados ...\n')
-    }
-    if(api_connection$status_code != 200){
-      message('Problemas na conexao \nTentando acessar a API novamente ...\n')
-      Sys.sleep(2)
-      api_connection = request(base_url = url) %>% req_perform()
-    }
-    if(api_connection$status_code != 200){
-      message('Problemas na conexao \nTentando acessar a API novamente ...\n')
-      Sys.sleep(5)
-      api_connection = request(base_url = url) %>% req_perform()
-    }
-    if(api_connection$status_code != 200){
-      message('Problemas na conexao \nTentando acessar a API uma última vez ...\n')
-      Sys.sleep(10)
-      api_connection = request(base_url = url) %>% req_perform()
-    }
-    else{'Falha na conexao :(\nTente conectar com a API mais tarde'}    
-  
+      dlg_message(message = 'Conexao bem sucedida ! \nDados sendo coletados ...\n', type  = 'ok')
+    } 
+    else if(api_connection$status_code != 200){
+      while(api_connection$status_code != 200 & flag <= 3){
+        flag = flag + 1
+        
+        if(flag == 1){
+          Sys.sleep(2)
+          message('Problemas na conexao. \nTentando acessar a API novamente ...\n')}
+        if(flag == 2){
+          Sys.sleep(5)
+          message('Problemas na conexao. \nTentando acessar a API novamente ...\n')}
+        if(flag == 3){
+          Sys.sleep(10)
+          message('Problemas na conexao. ! \nTentando acessar a API uma última vez ...\n')}
+        
+        api_connection = request(base_url = url) %>% req_perform()
+      }
       
+      ifelse(api_connection$status_code == 200,
+             dlg_message(message = 'Conexao bem sucedida ! \nDados sendo coletados ...\n', type = 'ok'),
+             dlg_message(message = 'Falha na conexao ! \nTente conectar com a API mais tarde.', type = 'ok')
+      )
+    }
+
+    
     # -- Conversao dos Dados para Formato Legivel  -- #
     api_connection = rawToChar(api_connection$body)                 # Conversao dos dados brutos (Raw) para formato de lista (JSon)
     api_connection = fromJSON(api_connection, flatten = TRUE)       # Conversao dos dados em formato de lista (Json) para formato de tabela (Data Frame)
