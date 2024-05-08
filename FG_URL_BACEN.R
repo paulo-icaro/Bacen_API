b41082bbdfeec8f73c12bab643fb46e4c66165c7
Paulo Icaro <87305764+paulo-icaro@users.noreply.github.com>
2024-05-08 14:59:30 -0300

    Adição do link das URL's modelo

--- FG_URL_BACEN.R
+++ FG_URL_BACEN.R
@@ -6,6 +6,16 @@
 
 
 
+# -------------------- #
+# --- URL's Modelo --- #
+# -------------------- #
+# url_ipca_br = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json&dataInicial=01/01/2003&dataFinal=01/12/2023'
+# url_ipca_ne = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.13921/dados?formato=json&dataInicial=01/01/2003&dataFinal=01/12/2023'
+# url_ipca_for = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.13006/dados?formato=json&dataInicial=01/01/2003&dataFinal=01/12/2023'
+
+
+
+
 # ------------------------------ #
 # --- Funcao Geradora de URL --- #
 # ------------------------------ #
