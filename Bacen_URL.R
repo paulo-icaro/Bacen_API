42ed431fe91280d9d5943f1b4ae7da1a3713aeec
pauloicaro <pauloicaro38@gmail.com>
2024-08-28 14:31:25 -0300

    Ajuste pontual

--- Bacen_URL.R
+++ Bacen_URL.R
@@ -2,13 +2,13 @@
 # === URL's Generation Function - API BACEN === #
 # ============================================= #
 
-# --- Script by Paulo Icaro ---#
+# --- Script by Paulo Icaro --- #
 
 
 
-# -------------------- #
-# --- URL's Models --- #
-# -------------------- #
+# ------------------ #
+# --- URL Models --- #
+# ------------------ #
 # url_ipca_br = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json&dataInicial=01/01/2003&dataFinal=01/12/2023'
 # url_ipca_ne = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.13921/dados?formato=json&dataInicial=01/01/2003&dataFinal=01/12/2023'
 # url_ipca_for = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.13006/dados?formato=json&dataInicial=01/01/2003&dataFinal=01/12/2023'
